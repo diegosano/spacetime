31396789408b7b96e37265efbd49a723bcf2f67e
Diego Sano <diegocsano@outlook.com>
2023-05-19 01:16:09 -0300

    feat(web): refactor components and create example .env file

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import { User } from 'lucide-react'
-
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
-import Image from 'next/image'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignInButton } from '@/components/SignInButton'
 
 export default function Home() {
   return (
@@ -11,65 +11,13 @@ export default function Home() {
 
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-
-          <div className="max-w-[420px] space-y-4">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-600"
-            href=""
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com ❤ no NLW da{' '}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://rocketseat.com.br"
-            className="underline transition-colors hover:text-gray-100"
-          >
-            Rocketseat
-          </a>
-        </div>
+        <SignInButton />
+        <Hero />
+        <Copyright />
       </div>
 
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a className="underline hover:text-gray-50" href="">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </div>
   )
