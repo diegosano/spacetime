84bc27bea4e65ddc906e1dee318c53a1cf02daa3
Diego Sano <diegocsano@outlook.com>
2023-05-19 01:33:02 -0300

    feat(server): create auth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 
 import { memoriesRoutes } from './routes/memories'
+import fastifyJwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(fastifyJwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
 
 app
